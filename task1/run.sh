26273bbec6e70692fcaba3b68959c6700c2e9457
Habib <boymurodov200124@gmail.com>
2020-04-15 23:20:03 +0300

    6th commit

--- task1/run.sh
+++ task1/run.sh
@@ -3,12 +3,13 @@ file_extension="$2"
 output_folder="$3"
 output_archive_name="$4"
 
+cd -P "dir_for_backup"
 chmod + x "$0"
 $(mkdir $output_folder)
 
 for file in $(find "$dir_for_backup" -name "*.$file_extension")
 do
-	$(\cp --backup=numbered "$file" "$output_folder")
+	$(cp --backup=numbered "$file" "$output_folder")
 done
 $(tar -czf "$output_archive_name" "$output_folder")
 echo "done"
