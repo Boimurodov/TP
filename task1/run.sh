b71cfc33398507fda551748028c7ed002c5774d1
Habib <boymurodov200124@gmail.com>
2020-04-15 21:30:07 +0300

    5th commit

--- task1/run.sh
+++ task1/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cd -P $1
-mkdir $3 
-cp -r $1/*.$2 $1/*/*.$2 --force --backup=numbered $1/$3 
-tar -czfv $4 $3
-echo "done"
+#./run.sh <dir> <extension> <backup folder> <output archive name>
 
+dir_for_backup="$1"
+file_extension="$2"
+output_folder="$3"
+output_archive_name="$4"
+
+$(mkdir $output_folder)
+
+for file in $(find "$dir_for_backup" -name "*.$file_extension")
+do
+	$(cp --backup=numbered "$file" "$output_folder")
+done
+$(tar -czf "$output_archive_name" "$output_folder")
+echo "done"
